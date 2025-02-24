8ba1906dff716473c0616e1c80e4eecc82383ab9
Laura <lauraduttra1@gmail.com>
2025-02-24 16:18:10 -0300

    Initial commit

---
+++ objeto.js
@@ -0,0 +1,11 @@
+// Criando um objeto de aluno
+const aluno= {
+    nome: "Laura",
+    idade: 23,
+
+    estudar: () => console.log
+    (`${aluno.nome} está estudando. `),
+ };
+
+ console.log(aluno.nome);
+ aluno.estudar();
